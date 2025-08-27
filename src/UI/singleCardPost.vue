9ed8c40e51baa073ba4e2427f4639a0a7149b4e9
vlad <vlad3eko@gmail.com>
2025-08-28 00:00:15 +0300

    add SingleCardPost new component

---
+++ src/UI/singleCardPost.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="card-showContent">
+    <img v-bind:src="blog.imageUrl" alt="" id="card-img">
+    <post-card v-bind="blog">
+      <p class="card-style" slot="title">{{ blog.title }}</p>
+      <p class="card-style" slot="content">{{ blog.content }}</p>
+      <div slot="categoryFor" v-for="category in blog.categories">
+        <p class="card-style" slot="category">{{ category }}</p>
+      </div>
+      <p class="card-style" slot="author">Author: {{ blog.author }}</p>
+    </post-card>
+  </div>
+</template>
+
+<script>
+
+import PostCardConstructor from "../UI/postCardConstructor.vue";
+
+
+export default {
+  components: {
+    'post-card': PostCardConstructor,
+  },
+  props: ['blog'],
+  data() {
+    return {}
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/widgets/GetCardsList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="blog-news">
+    <div class="single-blog" v-for="blog in filteredBlogs" :key="blog.id">
+      <router-link v-bind:to="'blog/' + blog.id">
+        <single-card v-bind:blog="blog"/>
+      </router-link>
+      <div id="blog-controls">
+        <button>Fix Content</button>
+        <button>Public</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import searchMixins from "../mixins/searchMixins";
+import getCard from "../mixins/getCard";
+import filters from "../mixins/filters";
+import singleCardPost from "../UI/singleCardPost.vue";
+
+
+export default {
+  components: {
+    'single-card': singleCardPost,
+
+  },
+  data() {
+    return {
+      blogs: [],
+    }
+  },
+  computed: {},
+
+  mixins: [searchMixins, getCard, filters]
+}
+
+</script>
+
+<style scoped>
+* {
+  text-decoration: none;
+}
+</style>
